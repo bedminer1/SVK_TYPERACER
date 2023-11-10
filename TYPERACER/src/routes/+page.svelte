fd4bc00023799c655900ef11d97747f5e89121f1
bedminer1 <alexander.seah.min.rui@gmail.com>
2023-11-10 23:26:31 +0800

    added WPM calculator

--- TYPERACER/src/routes/+page.svelte
+++ TYPERACER/src/routes/+page.svelte
@@ -1,11 +1,21 @@
 <script>
     import { onMount } from 'svelte';
+
     export let data
     const { quotes } = data
 
     let textDisplay = quotes[Math.floor(Math.random() * 100)].quote
     let input
     let completedTyping = false
+    let timer = 0
+    let startedTyping = true
+    let displayedWPM = 0
+
+    $: wordsPerMinute = Math.floor(textDisplay.split(' ').length / timer * 100 * 60)
+
+    // if(displayedWPM < wordsPerMinute && completedTyping === false) {
+     displayedWPM = wordsPerMinute
+    // }
 
     function newQuote() {
         window.location.reload()
@@ -14,14 +24,20 @@
     function checkCorrect() {
         if (input !== textDisplay) {
             return
-        } else {
-            stopTimer()
+        } else if (input === textDisplay) {
+            displayedWPM = wordsPerMinute
             completedTyping = true
         }
     }
 
-    function stopTimer() {
+    function startTimer() {
+        startedTyping = true
 
+        if (startedTyping === true && timer === 0) {
+        let timerInterval = setInterval(() => {
+            timer++
+        }, 10);
+        } 
     }
 
 </script>
@@ -31,13 +47,13 @@
         <p class="text-lg">{textDisplay}</p>
     </div>
     
-    <form class="w-1/2" on:submit|preventDefault={checkCorrect}>
-    <input type="text" bind:value={input} id="textInput" class="text-black outline-none w-full mt-4 pl-2 h-8">
+    <form class="w-1/2" on:submit|preventDefault={checkCorrect} autocomplete="off">
+    <input type="text" on:keydown={startTimer} bind:value={input} id="textInput" class="text-black outline-none w-full mt-4 pl-2 h-8">
     </form>
-
-    <button on:click={newQuote}>Next</button>
+    <button on:click={newQuote} class="w-auto text-2xl hover:text-red-200">Next</button>
 
     {#if completedTyping}
         <p class="text-4xl">Good Job!</p>
+        <p class="text-3xl"> WPM: {displayedWPM}</p>
     {/if}
 </div>
