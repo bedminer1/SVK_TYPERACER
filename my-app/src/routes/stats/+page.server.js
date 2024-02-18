6fa807545177163dd004563c3aa475100377be69
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-02-18 17:39:02 +0800

    used env variables so my db is less like to get hacked lol

--- my-app/src/routes/stats/+page.server.js
+++ my-app/src/routes/stats/+page.server.js
@@ -1,10 +1,11 @@
 import PocketBase from 'pocketbase';
+import { SECRET_PASSWORD, SECRET_EMAIL, SECRET_URL } from '$env/static/private'
 
 export async function load() {
-	let SECRET_PASSWORD = 'BWseSz8QqbmP7C7';
-	let SECRET_EMAIL = 'alexander.seah.min.rui@gmail.com';
+	// let SECRET_PASSWORD = 'BWseSz8QqbmP7C7';
+	// let SECRET_EMAIL = 'alexander.seah.min.rui@gmail.com';
 
-	const pb = new PocketBase('https://bedsshop.pockethost.io');
+	const pb = new PocketBase(SECRET_URL);
 	await pb.admins.authWithPassword(SECRET_EMAIL, SECRET_PASSWORD);
 	const records = await pb.collection('wpmData').getFullList(200, {
 		sort: '-created'
--- my-app/src/routes/typing/+page.server.js
+++ my-app/src/routes/typing/+page.server.js
@@ -1,13 +1,10 @@
 import PocketBase from 'pocketbase'
-
+import { SECRET_PASSWORD, SECRET_EMAIL, SECRET_URL } from '$env/static/private'
 
 
 export const actions = {
     create: async ({ request }) => {
-        let SECRET_PASSWORD = 'BWseSz8QqbmP7C7'
-        let SECRET_EMAIL = 'alexander.seah.min.rui@gmail.com'
-
-        const pb = new PocketBase('https://bedsshop.pockethost.io')
+        const pb = new PocketBase(SECRET_URL)
         await pb.admins.authWithPassword(SECRET_EMAIL, SECRET_PASSWORD)
 
         const form = await request.formData()
